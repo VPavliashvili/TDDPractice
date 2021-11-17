cbb75e7f960e32167d972505f026ec3d9e38a683
chesterapavliashvili <chesterapavliashvili@gmail.com>
2021-11-17 20:36:54 +0400

    separated stack implementation on another branch

---
+++ Stack/IStack.cs
@@ -0,0 +1,26 @@
+﻿namespace MyStack
+{
+
+    public interface IStack
+    {
+        int Count { get; }
+        object Peek { get; }
+
+        bool IsFull();
+        bool IsEmpty();
+        void Push(object value);
+        object Pop();
+    }
+
+    public interface IStackFactory
+    {
+        IStack CreateStack();
+        IStack CreateStack(int capacity);
+    }
+
+    public class StackFactory : IStackFactory
+    {
+        public IStack CreateStack() => new FixedSizeStack(byte.MaxValue);
+        public IStack CreateStack(int n) => new FixedSizeStack(n);
+    }
+}
